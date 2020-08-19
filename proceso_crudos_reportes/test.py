16449a372e47962ed4e6082affb4c67ee74e490f
pelos <juavig@gmail.com>
2020-08-18 21:59:25 -0300

    Update test.py

--- proceso_crudos_reportes/test.py
+++ proceso_crudos_reportes/test.py
@@ -1,9 +1,11 @@
 import json
 import datetime
 
-
-with open("test","r") as archivo:
+contador = 0
+with open("./proceso_crudos_reportes/test-heavy.ndjson","r") as archivo:
     archivo = archivo.read().splitlines()
     for linea in archivo:
         linea = json.loads(linea)
         print(linea["host"],linea["name"],datetime.datetime.fromtimestamp(linea["clock"]).strftime('%c'),int(linea["avg"])/1024/1024,int(linea['max'])/1024/1024)
+        contador = contador + 1
+print("Lineas Procesadas",contador)

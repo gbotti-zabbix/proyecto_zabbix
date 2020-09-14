87427a1d3497b575fe8a4853acd4ff5328ce0f8d
Pelos17 <34128856+Pelos17@users.noreply.github.com>
2020-09-14 12:47:01 -0300

    Update pusheobd.py

--- proceso_crudos_reportes/pusheobd.py
+++ proceso_crudos_reportes/pusheobd.py
@@ -3,7 +3,7 @@ import mysql.connector
 mydb = mysql.connector.connect(host="192.168.211.4",user="reportes",password="antel2020",database="diarios_crudos")
 mycursor = mydb.cursor()
 
-sql = "INSERT INTO `crudos` (`nodo`, `puerto`, `direccion`, `hora`, `fecha`, `promedio`, `pico`) VALUES ('{}','{}','{}','{}','{}',{},{})".format("Nodo2",'Puerto3','TX','23:30:00','2020-08-21',16.5,21.2)
+sql = "INSERT INTO `crudos` (`nodo`, `puerto`, `direccion`, `hora`, `fecha`, `promedio`, `pico`) VALUES ('{}','{}','{}','{}','{}',{},{})".format("Nodo2",'Puerto4','RX','23:30:00','2020-08-21',16.5,21.2)
 #sql = "INSERT INTO `Prueba` (`test1`, `test2`) VALUES ('check1','check2')"
 mycursor.execute(sql)
 
@@ -11,5 +11,4 @@ mydb.commit()
 
 print(mycursor.rowcount, "Ingresado.")
 
-
-#directo de phpmyadmin sql INSERT INTO `crudos`(`nodo`, `puerto`, `direccion`, `hora`, `fecha`, `promedio`, `pico`) VALUES ('Nodo2','Puerto3','TX','23:00:00','2020-08-20',15.2,20.2) 
+print("1 record inserted, ID:", mycursor.lastrowid) 

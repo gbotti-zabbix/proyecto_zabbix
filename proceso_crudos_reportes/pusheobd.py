c554cc6a0e5f0d88ba0fc722c73f9ab159cd37ec
Pelos17 <34128856+Pelos17@users.noreply.github.com>
2020-09-17 13:19:52 -0300

    Update pusheobd.py

--- proceso_crudos_reportes/pusheobd.py
+++ proceso_crudos_reportes/pusheobd.py
@@ -1,16 +1,45 @@
+# import mysql.connector
+# import csv
+# import pickle
+
+# #coneccion a la BD
+# mydb = mysql.connector.connect(host="192.168.211.4",user="reportes",password="antel2020",database="diarios_crudos")
+# mycursor = mydb.cursor()
+
+# with open ('./proceso_crudos_reportes/test-pickle2', 'rb') as lista:
+#     lista_tuplas = pickle.load(lista)
+#     sql = "INSERT INTO `crudos` (`nodo`, `puerto`, `direccion`, `hora`, `fecha`, `promedio`, `pico`) VALUES (%s,%s,%s,%s,%s,%s,%s)"
+#     mycursor.executemany(sql, lista_tuplas)
+
+#     mydb.commit()
+
+#     print(mycursor.rowcount, "Ingresado.")
+
+#!/usr/bin/python
+from datetime import date
 import mysql.connector
 import csv
 import pickle
 
+archivo_pickle = "./proceso_crudos_reportes/test-pickle"
+
 #coneccion a la BD
-mydb = mysql.connector.connect(host="192.168.211.4",user="reportes",password="antel2020",database="diarios_crudos")
-mycursor = mydb.cursor()
+#mydb = mysql.connector.connect(host="localhost",user="reportes",password="antel2020",database="reportes_zabbix",connect_timeout=1000)
+#mycursor = mydb.cursor()
 
-with open ('./proceso_crudos_reportes/test-pickle2', 'rb') as lista:
+with open (archivo_pickle, 'rb') as lista:
+    #carga de lista
     lista_tuplas = pickle.load(lista)
-    sql = "INSERT INTO `crudos` (`nodo`, `puerto`, `direccion`, `hora`, `fecha`, `promedio`, `pico`) VALUES (%s,%s,%s,%s,%s,%s,%s)"
-    mycursor.executemany(sql, lista_tuplas)
+    #
+#coneccion con BD
+mydb = mysql.connector.connect(host="192.168.211.4",user="reportes",password="antel2020",database="reportes_zabbix")
+mycursor = mydb.cursor()
+#
 
-    mydb.commit()
+#pusheo
+sql = "INSERT INTO `crudos_diarios` (`nodo`, `puerto`, `direccion`, `hora`, `fecha`, `promedio`, `pico`) VALUES (%s,%s,%s,%s,%s,%s,%s)"
+mycursor.executemany(sql, lista_tuplas)
+mydb.commit()
+#
 
-    print(mycursor.rowcount, "Ingresado.")
+print(mycursor.rowcount, "Ingresado.")

cf91020ea55b9194a8f5d7978bc60969e5deb44e
Pelos17 <34128856+Pelos17@users.noreply.github.com>
2020-09-18 16:48:32 -0300

    Update pusheobd.py

--- proceso_crudos_reportes/pusheobd.py
+++ proceso_crudos_reportes/pusheobd.py
@@ -1,45 +1,36 @@
-# import mysql.connector
-# import csv
-# import pickle
-
-# #coneccion a la BD
-# mydb = mysql.connector.connect(host="192.168.211.4",user="reportes",password="antel2020",database="diarios_crudos")
-# mycursor = mydb.cursor()
-
-# with open ('./proceso_crudos_reportes/test-pickle2', 'rb') as lista:
-#     lista_tuplas = pickle.load(lista)
-#     sql = "INSERT INTO `crudos` (`nodo`, `puerto`, `direccion`, `hora`, `fecha`, `promedio`, `pico`) VALUES (%s,%s,%s,%s,%s,%s,%s)"
-#     mycursor.executemany(sql, lista_tuplas)
-
-#     mydb.commit()
-
-#     print(mycursor.rowcount, "Ingresado.")
-
 #!/usr/bin/python
-from datetime import date
 import mysql.connector
-import csv
 import pickle
+from datetime import datetime, date
 
-archivo_pickle = "./proceso_crudos_reportes/test-pickle"
+print(datetime.now())
+archivo_pickle = "/var/lib/reportes-zabbix/Merged-Trends-" + str(date.today()) + ".pickle"
+contador_insert = 0
+lista_final = []
+contador_final = []
+sql = "INSERT INTO `crudos_diarios` (`nodo`, `puerto`, `direccion`, `hora`, `fecha`, `promedio`, `pico`) VALUES (%s,%s,%s,%s,%s,%s,%s)"
 
-#coneccion a la BD
-#mydb = mysql.connector.connect(host="localhost",user="reportes",password="antel2020",database="reportes_zabbix",connect_timeout=1000)
-#mycursor = mydb.cursor()
 
 with open (archivo_pickle, 'rb') as lista:
     #carga de lista
     lista_tuplas = pickle.load(lista)
     #
-#coneccion con BD
+
 mydb = mysql.connector.connect(host="192.168.211.4",user="reportes",password="antel2020",database="reportes_zabbix")
 mycursor = mydb.cursor()
-#
 
-#pusheo
-sql = "INSERT INTO `crudos_diarios` (`nodo`, `puerto`, `direccion`, `hora`, `fecha`, `promedio`, `pico`) VALUES (%s,%s,%s,%s,%s,%s,%s)"
-mycursor.executemany(sql, lista_tuplas)
+for dato in lista_tuplas:
+    lista_final.append(dato)
+    contador_insert = contador_insert + 1
+    if contador_insert == 100000:
+        mycursor.executemany(sql, lista_final)
+        mydb.commit()
+        contador_final.append(mycursor.rowcount)
+        contador_insert = 0
+        lista_final.clear()
+
+mycursor.executemany(sql, lista_final)
 mydb.commit()
-#
-
-print(mycursor.rowcount, "Ingresado.")
+contador_final.append(mycursor.rowcount)
+print("Total Ingresado",sum(contador_final))
+print(datetime.datetime.now())

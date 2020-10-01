149d6dfec633d019bb61ae5a7534b2f1d3c0ac5b
Juan Vignolo - Laptop <juavig@gmail.com>
2020-10-01 11:54:02 -0300

    Create pruebas.py

---
+++ proceso_crudos_reportes/pruebas.py
@@ -0,0 +1,10 @@
+
+# from datetime import datetime
+
+# now = datetime.now()
+
+# for x in range(0,100):
+#     while x >= 80:
+#         print
+# current_time = now.strftime("%H:%M:%S")
+# print(type(current_time))

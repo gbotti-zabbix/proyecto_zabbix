13886b505f1c82348f4ec38aba0c7a5d66bda3cc
Juan Vignolo - Laptop <juavig@gmail.com>
2020-10-02 10:13:04 -0300

    Update reportes_ONT.py

--- proceso_crudos_reportes/reportes_ONT.py
+++ proceso_crudos_reportes/reportes_ONT.py
@@ -12,57 +12,59 @@ lista_puerto_etiqueta = []
 def parseo_ont():
     contador_carga = 0
     contador_error = 0
+    #abro el archivo en read y separo en listas de json
     with open("test-heavy.ndjson","r") as crudo:
-        crudo = crudo.read().splitlines()
-        for linea in crudo:
-            linea = json.loads(linea)
-            #Arranca parseo
-            if "ONT" in linea["applications"]:
-                #Tipo
-                if "C300"  in linea["groups"]:
-                    Tipo = "C300"
-                elif "MA5800" in linea["groups"]:
-                    Tipo = "MA5800"
+        #archivo parseado
+        with open("test-pickle-ont","wb") as archivo_pickle:
+            crudo = crudo.read().splitlines()
+            #
+            
+            for linea in crudo:
+                #cada linea se pasa de json a dicc
+                linea = json.loads(linea)
                 #
-                #Nodo
-                Nodo = linea["host"]
-                #
-                #Nombre
-                Nombre = linea["name"][13:-5]
-                #
-                #Direccion
-                Direccion = linea["name"][-2:]
-                #
-                #Fecha y hora salen a partir de procesar tiempo
-                Tiempo = datetime.fromtimestamp(linea["clock"]).strftime('%Y-%m-%d %H:%M:%S').split()
-                #
-                #Paso los bits a mega para picos y avg
-                Promedio = float(linea["avg"])/1024/1024
-                Pico = float(linea['max'])/1024/1024
-                #
-                #Puerto (que dios me perdone)
-                for x in lista_puerto_etiqueta:
-                    if Nombre == x[0]:
-                        Puerto = x[1]
-                #
-                #termine de extraer datos
 
-                #creo lista de tuplas para picklear y ademas filtro
-                if (Pico < 2500 and Promedio < 2500):
-                    tupla = (Tipo,Nodo,Nombre,Puerto,Direccion,Tiempo[1],Tiempo[0],Promedio,Pico)
-                    #Test
-                    #tupla = (Tipo,Nodo,Nombre,Direccion,Tiempo[1],Tiempo[0],Promedio,Pico)
-                    lista_tuplas.append(tupla)
-                    contador_carga = contador_carga + 1
-                else:
-                    contador_error = contador_error + 1
-                    pass
-        print("{} lineas ingresadas. {} lineas no ingresadas".format(contador_carga,contador_error))
-    #Par el fromateo deberia sacar, Tipo, Nodo, Puerto, Nombre, Direccion, Fecha, Hora, Max y AVG
-    #Falta
-    #Puerto
+                #Arranca parseo
+                if "ONT" in linea["applications"]:
+                    #Tipo
+                    if "C300"  in linea["groups"]:
+                        Tipo = "C300"
+                    elif "MA5800" in linea["groups"]:
+                        Tipo = "MA5800"
+                    #
+                    #Nodo
+                    Nodo = linea["host"]
+                    #
+                    #Nombre
+                    Nombre = linea["name"][13:-5]
+                    #
+                    #Direccion
+                    Direccion = linea["name"][-2:]
+                    #
+                    #Fecha y hora salen a partir de procesar tiempo
+                    Tiempo = datetime.fromtimestamp(linea["clock"]).strftime('%Y-%m-%d %H:%M:%S').split()
+                    #
+                    #Paso los bits a mega para picos y avg
+                    Promedio = float(linea["avg"])/1024/1024
+                    Pico = float(linea['max'])/1024/1024
+                    #
+                    #Puerto (que dios me perdone), lista_puerto_etiqueta viene de la funcion puerto_etiqueta
+                    for x in lista_puerto_etiqueta:
+                        if Nombre == x[0]:
+                            Puerto = x[1]
+                    #
+                    #termine de extraer datos
 
-
+                    #creo lista de tuplas para picklear y ademas filtro
+                    if (Pico < 2500 and Promedio < 2500):
+                        tupla = (Tipo,Nodo,Nombre,Puerto,Direccion,Tiempo[1],Tiempo[0],Promedio,Pico)
+                        lista_tuplas.append(tupla)
+                        contador_carga = contador_carga + 1
+                    else:
+                        contador_error = contador_error + 1
+                        pass
+            pickle.dump(lista_tuplas,archivo_pickle)
+    print("{} lineas ingresadas. {} lineas no ingresadas".format(contador_carga,contador_error))
 
 def puerto_etiqueta():
     with open ("datos_puertos_RBS","r") as puertos_RBS:
@@ -78,6 +80,3 @@ def puerto_etiqueta():
 
 puerto_etiqueta()
 parseo_ont()
-
-
-print(lista_tuplas)

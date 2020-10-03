1737cb8bf6bd341b3948ae93f65c2c5761367107
Juan Vignolo - Laptop <juavig@gmail.com>
2020-10-03 18:56:53 -0300

    Update pruebas.py

--- proceso_crudos_reportes/pruebas.py
+++ proceso_crudos_reportes/pruebas.py
@@ -8,3 +8,8 @@
 #         print
 # current_time = now.strftime("%H:%M:%S")
 # print(type(current_time))
+
+import subprocess
+proc = subprocess.Popen(["ping", "192.168.1.1"], stdout=subprocess.PIPE, universal_newlines=True, shell=True)
+(out, err) = proc.communicate()
+print (out)

e39e57f53d43909ef7d600503ba31265642100f2
Pelos17 <34128856+Pelos17@users.noreply.github.com>
2020-10-07 15:53:40 -0300

    Update Pico_Diario_INSERT.sql

--- proceso_crudos_reportes/SQL/Pico_Diario_INSERT.sql
+++ proceso_crudos_reportes/SQL/Pico_Diario_INSERT.sql
@@ -1,4 +1,4 @@
-insert into picos_diarios (tipo, nodo, puerto, direccion, hora, fecha, promedio, pico) select t1.tipo, t1.nodo, t1.puerto, t1.direccion, t1.hora, t1.fecha, t1.promedio, t1.pico 
+insert into picos_semanales (tipo, nodo, puerto, direccion, hora, fecha, promediohora, pico) select t1.tipo, t1.nodo, t1.puerto, t1.direccion, t1.hora, t1.fecha, t1.promedio, t1.pico 
                 from (
-    select t2.*,  row_number() over (partition by t2.tipo, t2.nodo, t2.puerto, t2.direccion  order by t2.pico desc)  as rn from crudos_diarios t2 
+    select t2.*,  row_number() over (partition by t2.tipo, t2.nodo, t2.puerto, t2.direccion  order by t2.pico desc)  as rn from picos_diarios t2 
 )t1 where t1.rn =1;

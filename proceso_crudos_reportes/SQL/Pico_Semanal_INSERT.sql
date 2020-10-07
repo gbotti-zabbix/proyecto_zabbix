9904d493d572815a4b4c38407cd54bd06696cc05
Pelos17 <34128856+Pelos17@users.noreply.github.com>
2020-10-07 16:03:28 -0300

    Update Pico_Semanal_INSERT.sql

--- proceso_crudos_reportes/SQL/Pico_Semanal_INSERT.sql
+++ proceso_crudos_reportes/SQL/Pico_Semanal_INSERT.sql
@@ -1,12 +1,6 @@
-insert into picos_semanales (tipo, nodo, puerto, direccion, hora, fecha, promediohora, promediopicos, pico)
-SELECT pd.tipo, pd.nodo, pd.puerto, pd.direccion, pd.hora, pd.fecha, pd.promedio as promediohora, promediopicos, pd.pico 
-FROM picos_diarios pd
-INNER JOIN
-	(SELECT puerto, direccion, nodo, AVG(pico) as promediopicos, MAX(pico) as MaxPico
-    FROM picos_diarios
-    GROUP BY puerto, direccion,nodo) groupedpd
-ON pd.puerto = groupedpd.puerto
-and pd.direccion = groupedpd.direccion
-and pd.nodo = groupedpd.nodo
-and pd.pico = groupedpd.MaxPico
-and promediopicos = groupedpd.promediopicos order by pico limit 10;
+insert into picos_semanales (tipo, nodo, puerto, direccion, hora, fecha, promedio, pico) select t1.tipo, t1.nodo, t1.puerto, t1.direccion, t1.hora,
+t1.fecha, t1.promedio, t1.pico
+                from ( select t2.*, row_number() over (partition by t2.tipo, t2.nodo, t2.puerto, t2.direccion order by t2.pico desc) as rn from picos_diarios
+    t2
+)t1 where t1.rn =1;
+

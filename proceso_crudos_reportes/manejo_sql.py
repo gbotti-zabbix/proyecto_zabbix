9ad01fb4f3bde2ca24934d4809d4c537c3f2ae26
Pelos17 <34128856+Pelos17@users.noreply.github.com>
2020-10-08 11:38:52 -0300

    Create manejo_sql.py

---
+++ proceso_crudos_reportes/manejo_sql.py
@@ -0,0 +1,33 @@
+def conector():
+    pass
+
+def insert_picos_diarios_semanal():
+    pass
+
+def insert_picos_diarios_mensual():
+    pass
+
+def insert_picos_semanal():
+    pass
+
+def insert_picos_mensual():
+    pass
+
+def insert_promedio_semanal():
+    pass
+
+def insert_promedio_mensual():
+    pass
+
+def insert_reporte_semanal():
+    pass
+
+def insert_reporte_mensual():
+    pass
+
+def insert_resaldo_semanal():
+    pass
+
+def insert_resaldo_mensual():
+    pass
+

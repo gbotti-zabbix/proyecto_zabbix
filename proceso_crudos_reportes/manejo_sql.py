815dca3a5a4263ab2c487135c8bf5dde2c0663c1
Pelos17 <34128856+Pelos17@users.noreply.github.com>
2020-10-09 08:30:23 -0300

    Update manejo_sql.py

--- proceso_crudos_reportes/manejo_sql.py
+++ proceso_crudos_reportes/manejo_sql.py
@@ -1,8 +1,37 @@
-def conector():
-    pass
+import mysql.connector
 
-def insert_picos_diarios_semanal():
-    pass
+def conector():
+    mydb = mysql.connector.connect(host="192.168.211.4",user="reportes",password="antel2020",database="reportes_zabbix")
+    return mydb
+
+def insert_picos_diarios_semanal(mydb):
+    sql = "INSERT INTO `test_picos` (`tipo`,`nodo`, `puerto`, `direccion`, `hora`,`pico`) VALUES ('C600','N-PALMIRA-01Z','18/7','TX','13:00:00',58)"
+    mydb.cursor().execute(sql)
+    mydb.commit()
+    print(mydb.cursor().rowcount)
+    mydb.cursor().close()
+    mydb.close()
+
+def menu():
+    print("""* 1 para pico_diario_semanal
+    * 2 para conector""")
+    selector = int(input("Que quiere hacer:"))
+    if selector == 1:
+        insert_picos_diarios_semanal(conector())
+    elif selector == 2:
+        conector()
+
+menu ()
+
+
+
+
+
+
+
+
+
+
 
 def insert_picos_diarios_mensual():
     pass
@@ -30,4 +59,3 @@ def insert_resaldo_semanal():
 
 def insert_resaldo_mensual():
     pass
-

f28855ec535f31a777bf8df799f020877a8a70d5
Pelos17 <juavig@gmail.com>
2020-10-27 17:36:19 -0300

    Update ingreso_inventario.py

--- API/gnomes/ingreso_inventario.py
+++ API/gnomes/ingreso_inventario.py
@@ -10,7 +10,7 @@ with open ("datos_central.csv","r") as archivo:
        	lista.append(x.split(";"))
     	#Uso las listas dentro de lista para llamar a la api por cada nodo"
     for y in lista:
-        process = subprocess.run(["./zhostupdater.py","-u","jvignolo","-p","brisingr","-a","http://127.0.0.1/zabbix/api_jsonrpc.php",y[0],"-I","tag={}".format(y[1])],stdout=subprocess.PIPE,universal_newlines=True,)
+        process = subprocess.run(["./zhostupdater.py","-u","jvignolo","-p","brisingr","-a","http://10.0.0.101/zabbix/api_jsonrpc.php",y[0],"-I","tag={}".format(y[1])],stdout=subprocess.PIPE,universal_newlines=True,)
         #Meto el sleep para no quebrar el SV
         time.sleep(2)
 

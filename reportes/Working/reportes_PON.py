00d69bd2e7b06721a178e0f43619b6d66d26bef6
Pelos17 <juavig@gmail.com>
2020-10-28 15:09:04 -0300

    Update reportes_PON.py

--- reportes/Working/reportes_PON.py
+++ reportes/Working/reportes_PON.py
@@ -14,7 +14,7 @@ def parsea_crudos():
     archivo = "/var/lib/reportes-zabbix/Merged-Trends-" + str(date.today()) + ".ndjson"
     archivo_pickle = "/var/lib/reportes-zabbix/crudos/Merged-Trends-" + str(date.today()) + ".pickle"
 
-#abro el archivo en read y separo en listas de json, descomentar el basico o el hevy
+    #abro el archivo en read y separo en listas de json, descomentar el basico o el hevy
     with open(archivo,"r") as archivo:
         #archivo parseado
         with open(archivo_pickle,"wb") as archivo2:

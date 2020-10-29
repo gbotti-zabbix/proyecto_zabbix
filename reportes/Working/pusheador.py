d093c02354dc7d97ca103d92d5f7bbcb3ca1c45e
Pelos17 <juavig@gmail.com>
2020-10-29 13:11:03 -0300

    Update pusheador.py

--- reportes/Working/pusheador.py
+++ reportes/Working/pusheador.py
@@ -4,8 +4,37 @@ import mysql.connector
 import pickle
 from datetime import datetime, date
 
-def pusheo_crudos_diarios():
-    fecha_pickle = input("Que fecha de PON desea pushear?\n Formato: YYYY-MM-DD: ")
+def conector_insert(sql):
+    mydb = mysql.connector.connect(host="localhost",user="reportes",password="antel2020",database="reportes_zabbix")
+    cursor = mydb.cursor()
+    cursor.execute(sql)
+    mydb.commit()
+    print(cursor.rowcount)
+    mydb.cursor().close()
+    mydb.close()
+
+
+def insert_picos_diarios_semanal():
+    sql = "insert into picos_diarios_semanal (tipo, nodo, puerto, direccion, hora, fecha, promedio, pico) select t1.tipo, t1.nodo, t1.puerto, t1.direccion, t1.hora, t1.fecha, t1.promedio, t1.pico from ( select t2.*, row_number() over (partition by t2.tipo, t2.nodo, t2.puerto, t2.direccion order by t2.pico desc) as rn from crudos_diarios t2) t1 where t1.rn =1;"
+    return sql
+
+
+def insert_picos_diarios_mensual():
+    sql = "insert into picos_diarios_mensual (tipo, nodo, puerto, direccion, hora, fecha, promedio, pico) select t1.tipo, t1.nodo, t1.puerto, t1.direccion, t1.hora, t1.fecha, t1.promedio, t1.pico from ( select t2.*, row_number() over (partition by t2.tipo, t2.nodo, t2.puerto, t2.direccion order by t2.pico desc) as rn from crudos_diarios t2) t1 where t1.rn =1;"
+    return sql
+
+
+def insert_picos_diarios_semanal_ont():
+    sql = "insert into picos_diarios_semanal_ont (tipo, nodo, puerto, etiqueta, direccion, hora, fecha, promedio, pico) select t1.tipo, t1.nodo, t1.puerto, t1.etiqueta, t1.direccion, t1.hora, t1.fecha, t1.promedio, t1.pico from ( select t2.*, row_number() over (partition by t2.tipo, t2.nodo, t2.puerto, t2.direccion order by t2.pico desc) as rn from crudos_diarios_ont t2) t1 where t1.rn =1;"
+    return sql
+
+
+def insert_picos_diarios_mensual_ont():
+    sql = "insert into picos_diarios_mensual_ont (tipo, nodo, puerto, etiqueta, direccion, hora, fecha, promedio, pico) select t1.tipo, t1.nodo, t1.puerto, t1.etiqueta, t1.direccion, t1.hora, t1.fecha, t1.promedio, t1.pico from ( select t2.*, row_number() over (partition by t2.tipo, t2.nodo, t2.puerto, t2.direccion order by t2.pico desc) as rn from crudos_diarios_ont t2) t1 where t1.rn =1;"
+    return sql
+
+
+def pusheo_crudos_diarios(fecha_pickle):
 
     #variables que uso mas adelante y consulta sql
     print(datetime.now())
@@ -41,9 +70,7 @@ def pusheo_crudos_diarios():
     print(datetime.now())
 
 
-def pusheo_crudos_diarios_ONT():
-
-    fecha_pickle = input("Que fecha de ONT desea pushear?\n Formato: YYYY-MM-DD: ")
+def pusheo_crudos_diarios_ONT(fecha_pickle):
 
     print(datetime.now())
 
@@ -68,7 +95,30 @@ def pusheo_crudos_diarios_ONT():
     print(datetime.now())
 
 #Menu
-if sys.argv[1] == "PON":
-    pusheo_crudos_diarios()
-elif sys.argv[1] == "ONT":
-    pusheo_crudos_diarios_ONT()
+
+tipo_pusheo = int(input("Desea Pushear:\n 1- PON\n2-ONT\n3-Cancelar\nIngrese opcion numerica: "))
+
+fecha_pickle = int(input("Que fecha desea pushear?\n Formato: YYYY-MM-DD: "))
+
+inserter = int(input("Quiere insertarlo en picos diarios?\nIngrese una opcion numerica\n1- No\n2- Semanales\n3- Mensual"))
+
+
+if tipo_pusheo == 1:
+    pusheo_crudos_diarios(fecha_pickle)
+    if inserter == 1:
+        break
+    elif inserter == 2:
+        conector_insert(insert_picos_diarios_semanal())
+    elif inserter == 3:
+        conector_insert(insert_picos_diarios_mensual())
+
+elif tipo_pusheo == 2:
+    pusheo_crudos_diarios_ONT(fecha_pickle)
+    if inserter == 1:
+        break
+    elif inserter == 2:
+        conector_insert(insert_picos_diarios_semanal_ont())
+    elif inserter == 3:
+        conector_insert(insert_picos_diarios_mensual_ont())
+elif tipo_pusheo == 3:
+    break

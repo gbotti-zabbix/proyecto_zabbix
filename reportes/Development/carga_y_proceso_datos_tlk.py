b34a5cd7d5272720810e6df3f9691df85fc87b2a
Pelos17 <juavig@gmail.com>
2020-11-03 15:15:15 -0300

    Update carga_y_proceso_datos_tlk.py

--- reportes/Development/carga_y_proceso_datos_tlk.py
+++ reportes/Development/carga_y_proceso_datos_tlk.py
@@ -5,18 +5,19 @@ import sys
 
 def conector_insert(sql):
     #mydb = mysql.connector.connect(host="localhost",user="reportes",password="antel2020",database="reportes_zabbix")
-    mydb = mysql.connector.connect (host="192.168.211.4",user="reportes",password="antel2020",database="reportes_zabbix")
+    mydb = mysql.connector.connect(host="192.168.211.4",user="reportes",password="antel2020",database="reportes_zabbix")
     cursor = mydb.cursor()
     cursor.execute(sql)
-    mydb.commit()
-    print(cursor.rowcount)
+    resultado = cursor.fetchall()
+    print (resultado)
     mydb.cursor().close()
     mydb.close()
 
 
 
 
-conector_insert ("select * from t_rbs_x_puerto ")
+
+conector_insert ("select * from t_rbs_x_puerto;")
 
 
 

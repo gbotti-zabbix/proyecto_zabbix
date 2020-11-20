b50616220d65b8f5cb07a27e6979969c0131b211
Pelos17 <juavig@gmail.com>
2020-11-20 16:37:00 -0300

    Create conector.py

---
+++ reportes/Refactor/conector.py
@@ -0,0 +1,21 @@
+import mysql.connector
+import logging
+
+def conector(sql,tipo,mensaje,*args):
+    mydb = mysql.connector.connect(host="localhost",user="reportes",password="antel2020",database="reportes_zabbix")
+    logging.info( f' Ejecutando: {tipo_sql}')
+    cursor = mydb.cursor()
+    if tipo == "many":
+        cursor.executemany(sql, args[0])
+    elif tipo == "select":
+        resultado = cursor.fetchall()
+        mydb.cursor().close()
+        mydb.close()
+        return resultado
+    else:
+        cursor.execute(sql)
+    mydb.commit()
+    logging.info (f' Succuessfully loaded: {cursor.rowcount} affected rows ')
+    print(cursor.rowcount)
+    cursor.close()
+    mydb.close()

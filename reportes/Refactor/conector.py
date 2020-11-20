78416f9a1ceb0cd2254a5c7082d7389e84d478f1
Pelos17 <juavig@gmail.com>
2020-11-20 16:37:46 -0300

    Update conector.py

--- reportes/Refactor/conector.py
+++ reportes/Refactor/conector.py
@@ -1,6 +1,7 @@
 import mysql.connector
 import logging
 
+# Se le pasa query, tipo de query, mensaje para log, y lista si es tipo many
 def conector(sql,tipo,mensaje,*args):
     mydb = mysql.connector.connect(host="localhost",user="reportes",password="antel2020",database="reportes_zabbix")
     logging.info( f' Ejecutando: {tipo_sql}')

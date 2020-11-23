73c2fd7a6a5887fbeb1ab432c7e5781d8b4f446d
gbotti-zabbix <72513868+gbotti-zabbix@users.noreply.github.com>
2020-11-23 14:41:37 -0300

    Update conector.py

--- reportes/Refactor/conector.py
+++ reportes/Refactor/conector.py
@@ -1,10 +1,14 @@
 import mysql.connector
 import logging
 
+#----importación variables de configuración
+import cfg
+
+
 # Se le pasa query, tipo de query, mensaje para log, y lista si es tipo many
 def conector(sql,tipo,mensaje,*args):
-    mydb = mysql.connector.connect(host="localhost",user="reportes",password="antel2020",database="reportes_zabbix")
-    logging.info( f' Ejecutando: {tipo_sql}')
+    mydb = mysql.connector.connect(host=cfg.host_DB,user=cfg.user_DB,password=cfg.password_DB,database=cfg.database_DB)
+    logging.info( f'Ejecutando: {tipo_sql}')
     cursor = mydb.cursor()
     if tipo == "many":
         cursor.executemany(sql, args[0])
@@ -16,7 +20,7 @@ def conector(sql,tipo,mensaje,*args):
     else:
         cursor.execute(sql)
     mydb.commit()
-    logging.info (f' Succuessfully loaded: {cursor.rowcount} affected rows ')
+    logging.info (f'Succuessfully loaded: {cursor.rowcount} affected rows')
     print(cursor.rowcount)
     cursor.close()
     mydb.close()

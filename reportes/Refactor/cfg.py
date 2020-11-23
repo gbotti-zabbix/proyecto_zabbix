6ce62194c35e16a87daaaae8290fcdc8fc2b2b8a
gbotti-zabbix <72513868+gbotti-zabbix@users.noreply.github.com>
2020-11-23 13:12:43 -0300

    Create cfg.py

---
+++ reportes/Refactor/cfg.py
@@ -0,0 +1,25 @@
+
+
+#========variables globales=========
+
+#---   LOGS  --------------
+file_log= "/var/log/reportes_zabbix/proc_tlk_BD.log"
+
+
+#=======  Archivos inventarios y procesados=========
+
+# -directorio origen y destino de archivos
+path_files="/var/lib/reportes-zabbix/reporte_tlk/"  #directorio trabajo
+
+file_tlk="PLN245_procesado.TXT"               #nombre archivo origen
+file_tlk_dst="PLN245_pareseado.csv"              #nombre archivo destino
+file_tlk_old="PLN245_procesado.old.TXT"              #luego parseo renombro archivo original
+
+
+
+#=========dato base datos========================
+#-variables para conexión a la base de datos
+host_DB="localhost"
+user_DB="reportes"
+password_DB="antel2020"
+database_DB="reportes_zabbix"

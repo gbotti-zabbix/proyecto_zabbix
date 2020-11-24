f7924b57d39b530804b21a8e8fbca368546bbd66
Pelos17 <juavig@gmail.com>
2020-11-24 10:29:24 -0300

    Update test_botti.py

--- reportes/Refactor/test_botti.py
+++ reportes/Refactor/test_botti.py
@@ -3,5 +3,6 @@ from conector import conector
 
 sql = "SELECT nro_tlk,letra_gestion,modelo FROM t_diccionario_nodos_tlk;"
 comentario="Trer tipos nodos"
-print (conector(sql,comentario,"select"))
+respuesta = (conector(sql,comentario,"select"))
 
+print(respuesta)

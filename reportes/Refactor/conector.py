bb20f5023ec6876f200c37d75420ca1cee2ce487
gbotti-zabbix <72513868+gbotti-zabbix@users.noreply.github.com>
2020-11-24 13:27:27 -0300

    Update conector.py

--- reportes/Refactor/conector.py
+++ reportes/Refactor/conector.py
@@ -20,6 +20,6 @@ def conector(sql,tipo,mensaje,*args):
         cursor.execute(sql)
     mydb.commit()
     logger.info (f' Succuessfully loaded: {cursor.rowcount} affected rows ')
-    print(cursor.rowcount)
+    #print(cursor.rowcount)
     cursor.close()
     mydb.close()

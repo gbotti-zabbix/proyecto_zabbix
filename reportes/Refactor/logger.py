10508d4d1accf3c41e1b2b0363504ae666159e89
gbotti-zabbix <72513868+gbotti-zabbix@users.noreply.github.com>
2020-11-24 10:02:24 -0300

    Update logger.py

--- reportes/Refactor/logger.py
+++ reportes/Refactor/logger.py
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-import direcciones
-
-=======
 from direcciones import file_log
->>>>>>> 8fdc4c050faada466dcb02c21a7494b29fa8fca3
 from datetime import datetime, date, timedelta
 
 def error(mensaje):
@@ -14,7 +9,7 @@ def error(mensaje):
         log.write(logeo)
 
 def info(mensaje):
-    with open(direcciones.file_log,"a") as log:
+    with open(file_log,"a") as log:
         tipo = "INFO"
         hora = datetime.now()
         logeo = tipo + " " + str(hora) + " " + mensaje + "\n"

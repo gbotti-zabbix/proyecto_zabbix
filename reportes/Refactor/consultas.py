6ed7a264a0517cb647a224b8661dd2cd10b28648
Pelos17 <juavig@gmail.com>
2020-12-07 14:51:54 -0300

    Create consultas.py

---
+++ reportes/Refactor/consultas.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+#Reporte Zabbix
+sql_ont_semanal = "SELECT * FROM reporte_semanal_ont order by pico DESC"
+sql_ont_mensual = "SELECT * FROM reporte_mensual_ont order by pico DESC"
+sql_pon_semanal = "SELECT * FROM reporte_semanal order by pico DESC"
+sql_pon_mensual = "SELECT * FROM reporte_mensual order by pico DESC"

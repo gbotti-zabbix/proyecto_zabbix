254ca714b0b4d5841bfce37d1c63ece343e09b23
Pelos17 <juavig@gmail.com>
2020-12-09 11:47:32 -0300

    Update consultas.py

--- reportes/Refactor/consultas.py
+++ reportes/Refactor/consultas.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python
 
+#pusheo zabbix
+sql_truncate_cdiarios_PON = 'TRUNCATE crudos_diarios;'
+sql_truncate_cdiarios_ONT = 'TRUNCATE crudos_diarios_ont;'
+sql_push_diarios_ONT = "INSERT INTO `crudos_diarios_ont` (`tipo`,`nodo`, `puerto`, `direccion`, `etiqueta`, `hora`, `fecha`, `promedio`, `pico`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+sql_push_diarios_PON = "INSERT INTO `crudos_diarios` (`id_zabbix`,`id_tlk`,`tipo`,`nodo`, `puerto`, `direccion`, `hora`, `fecha`, `promedio`, `pico`) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+
+
 #Reporte Zabbix
 sql_ont_semanal = "SELECT * FROM reporte_semanal_ont order by pico DESC"
 sql_ont_mensual = "SELECT * FROM reporte_mensual_ont order by pico DESC"

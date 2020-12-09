8f09fbb4729968cc37bfea255255946a04847e2c
Pelos17 <juavig@gmail.com>
2020-12-09 17:16:48 -0300

    Update flujo_db.py

--- reportes/Refactor/flujo_db.py
+++ reportes/Refactor/flujo_db.py
@@ -23,12 +23,12 @@ def flujo_semanal():
     conector(consultas.sql_truncate_promedio_semanal_pon,"Truncate","Truncando datos en promedio_semanal")
     conector(consultas.sql_truncate_picos_semanal_pon,"Truncate","Truncando datos en picos_semanal")
     conector(consultas.sql_truncate_reporte_semanal_pon,"Truncate","Truncando datos en reporte_semana")
-    conector(consultas.sql_truncate_respaldo_semanal_pon,"Truncate","Truncando datos en respaldo_semanal")
+    conector(consultas.sql_truncate_respaldo_semanal_pon,"Truncate","Truncando datos en respaldo_reporte_semanal")
     #Flujo
     conector(consultas.sql_insert_promedio_semanal_pon,"Flujo","Insertando datos en promedio_semanal")
     conector(consultas.sql_insert_picos_semanal_pon,"Flujo","Insertando datos en picos_semanal")
     conector(consultas.sql_insert_reporte_semanal_pon,"Flujo","Insertando datos en reporte_semanal")
-    conector(consultas.sql_insert_respaldo_semanal_pon,"Flujo","Insertando datos en respaldo_semanal")
+    conector(consultas.sql_insert_respaldo_semanal_pon,"Flujo","Insertando datos en respaldo_reporte_semanal")
     #Truncate_2
     conector(consultas.sql_truncate_picos_diarios_semanal_pon,"Truncate","Truncando datos en diarios_semanal")
 
@@ -37,12 +37,12 @@ def flujo_semanal():
     conector(consultas.sql_truncate_promedio_semanal_ont,"Truncate","Truncando datos en promedio_semanal_ont")
     conector(consultas.sql_truncate_picos_semanal_ont,"Truncate","Truncando datos en picos_semanal_ont")
     conector(consultas.sql_truncate_reporte_semanal_ont,"Truncate","Truncando datos en reporte_semanal_ont")
-    conector(consultas.sql_truncate_respaldo_semanal_ont,"Truncate","Truncando datos en respaldo_semanal_ont")
+    conector(consultas.sql_truncate_respaldo_semanal_ont,"Truncate","Truncando datos en respaldo_reporte_semanal_ont")
     #Flujo
     conector(consultas.sql_insert_promedio_semanal_ont,"Flujo","Insertando datos en promedio_semanal_ont")
     conector(consultas.sql_insert_picos_semanal_ont,"Flujo","Insertando datos en picos_semanal_ont")
     conector(consultas.sql_insert_reporte_semanal_ont,"Flujo","Insertando datos en reporte_semanal_ont")
-    conector(consultas.sql_insert_respaldo_semanal_ont,"Flujo","Insertando datos en respaldo_semanal_ont")
+    conector(consultas.sql_insert_respaldo_semanal_ont,"Flujo","Insertando datos en respaldo_reporte_semanal_ont")
     #Truncate_2
     conector(consultas.sql_truncate_picos_diarios_semanal_ont,"Truncate","Truncando datos en diarios_semanal_ont")
     logger.info("Finalizo el flujo semanal de la BD")
@@ -53,29 +53,29 @@ def flujo_mensual():
     ##PON
     #Truncate_1
     
-    conector(consultas.sql_truncate_promedio_mensual_pon,"Truncate","")
-    conector(consultas.sql_truncate_picos_mensual_pon,"Truncate","")
-    conector(consultas.sql_truncate_reporte_mensual_pon,"Truncate","")
-    conector(consultas.sql_truncate_respaldo_mensual_pon,"Truncate","")
+    conector(consultas.sql_truncate_promedio_mensual_pon,"Truncate","Truncando datos en promedio_mensual")
+    conector(consultas.sql_truncate_picos_mensual_pon,"Truncate","Truncando datos en picos_mensual")
+    conector(consultas.sql_truncate_reporte_mensual_pon,"Truncate","Truncando datos en reporte_mensual")
+    conector(consultas.sql_truncate_respaldo_mensual_pon,"Truncate","Truncando datos en respaldo_reporte_mensual")
     #Flujo
-    conector(consultas.sql_insert_promedio_mensual_pon,"Flujo","")
-    conector(consultas.sql_insert_picos_mensual_pon,"Flujo","")
-    conector(consultas.sql_insert_reporte_mensual_pon,"Flujo","")
-    conector(consultas.sql_insert_respaldo_mensual_pon,"Flujo","")
+    conector(consultas.sql_insert_promedio_mensual_pon,"Flujo","Insertando datos en promedio_mensual")
+    conector(consultas.sql_insert_picos_mensual_pon,"Flujo","Insertando datos en picos_mensual")
+    conector(consultas.sql_insert_reporte_mensual_pon,"Flujo","Insertando datos en reporte_mensual")
+    conector(consultas.sql_insert_respaldo_mensual_pon,"Flujo","Insertando datos en respaldo_reporte_mensual")
     #Truncate_2
     conector(consultas.sql_truncate_picos_diarios_mensual_pon,"Truncate","Truncando datos en picos_diarios_mensual")
 
     ##ONT
     #Truncate_1
-    conector(consultas.sql_truncate_promedio_mensual_ont,"Truncate","")
-    conector(consultas.sql_truncate_picos_mensual_ont,"Truncate","")
-    conector(consultas.sql_truncate_reporte_mensual_ont,"Truncate","")
-    conector(consultas.sql_truncate_respaldo_mensual_ont,"Truncate","")
+    conector(consultas.sql_truncate_promedio_mensual_ont,"Truncate","Truncando datos en promedio_mensual_ont")
+    conector(consultas.sql_truncate_picos_mensual_ont,"Truncate","Truncando datos en picos_mensual_ont")
+    conector(consultas.sql_truncate_reporte_mensual_ont,"Truncate","Truncando datos en reporte_mensual_ont")
+    conector(consultas.sql_truncate_respaldo_mensual_ont,"Truncate","Truncando datos en respaldo_reporte_mensual_ont")
     #Flujo
-    conector(consultas.sql_insert_promedio_mensual_ont,"Flujo","")
-    conector(consultas.sql_insert_picos_mensual_ont,"Flujo","")
-    conector(consultas.sql_insert_reporte_mensual_ont,"Flujo","")
-    conector(consultas.sql_insert_respaldo_mensual_ont,"Flujo","")
+    conector(consultas.sql_insert_promedio_mensual_ont,"Flujo","Insertando datos en promedio_mensual_ont")
+    conector(consultas.sql_insert_picos_mensual_ont,"Flujo","Insertando datos en picos_mensual_ont")
+    conector(consultas.sql_insert_reporte_mensual_ont,"Flujo","Insertando datos en reporte_mensual_ont")
+    conector(consultas.sql_insert_respaldo_mensual_ont,"Flujo","Insertando datos en respaldo_reporte_mensual_ont")
     #Truncate_2
     conector(consultas.sql_truncate_picos_diarios_mensual_ont,"Truncate","Truncando datos en picos_diarios_mensual_ont")
     logger.info("Finalizo el flujo mensual de la BD")

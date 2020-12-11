54db4469a8293455264bc17651a4fcd82cc03a41
Pelos17 <juavig@gmail.com>
2020-12-11 17:17:42 -0300

    Update direcciones.py

--- reportes/Refactor/direcciones.py
+++ reportes/Refactor/direcciones.py
@@ -20,10 +20,64 @@ file_tlk="PLN245_procesado.TXT"               #nombre archivo origen
 file_tlk_dst="PLN245_parseado.csv"              #nombre archivo destino
 file_tlk_old="PLN245_procesado.old.TXT"              #luego parseo renombro archivo original
 
+archivo_tlk = path_files+file_tlk
+archivo_tlk_dst = path_files+file_tlk_dst
+archivo_tlk_viejo = file_tlk+file_tlk_old
+
+file_rbs_DCS= "Boti te olvidaste de declarar esta variable"
+file_rbs_DCS_dst= "Esta tambien te olvidaste"
+file_rbs_DCS_old= "Me vas a tener que soltar la mano algun dia"
+
+archivo_rbs_DCS= path_files+file_rbs_DCS
+archivo_rbs_DCS_dst= path_files+file_rbs_DCS_dst
+archivo_rbs_DCS_old = path_files+file_rbs_DCS_old
+
 
 
 #=======  Inventarios Zabbix=========#
-crudozabbix = "/var/lib/reportes-zabbix/Merged-Trends-" + str(date.today()) + ".ndjson"
+def crudozabbix():
+    crudozabbix = "/var/lib/reportes-zabbix/Merged-Trends-" + str(date.today()) + ".ndjson"
+    return crudozabbix
+
+
+def archivo_pickle_ONT():
+    archivo_pickle_ONT = "/var/lib/reportes-zabbix/crudos/Merged-Trends-" + str(date.today()) + "_ONT.pickle"
+    return archivo_pickle_ONT
+
+
+def archivo_pickle_PON():
+    archivo_pickle_PON = "/var/lib/reportes-zabbix/crudos/Merged-Trends-" + str(date.today()) + ".pickle"
+    return archivo_pickle_PON
+
+#=======  Reporte Zabbix=========#
+
+def excel_PON_semanal():
+    excel_PON_semanal = "/var/lib/reportes-zabbix/reportes_semanales/Reporte_Semanal_" + str(date.today()) + ".xlsx"
+    return excel_PON_semanal
+
+def excel_PON_mensual():
+    excel_PON_mensual = "/var/lib/reportes-zabbix/reportes_mensuales/Reporte_Mensual_" + str(date.today()) + ".xlsx"
+    return excel_PON_mensual
+
+def excel_ONT_semanal():
+    excel_ONT_semanal = "/var/lib/reportes-zabbix/reportes_semanales/Reporte_Semanal_" + str(date.today()) + "_ONT.xlsx"
+    return excel_ONT_semanal
+
+def excel_ONT_mensual():
+    excel_ONT_mensual = "/var/lib/reportes-zabbix/reportes_mensuales/Reporte_Mensual_" + str(date.today()) + "_ONT.xlsx"
+    return excel_ONT_mensual
+
+class DefiniciónEncabezados:
+    def __init__(self, nombreHoja, encabezados):
+        self.nombreHoja = nombreHoja
+        self.encabezados = encabezados
+
+encabezados_PON_PON = {"A1":"Modelo Nodo","B1":"Nodo","C1":"Slot/Puerto","D1":"Hora Pico","E1":"Fecha Pico","F1":"Pico","G1":"% Utilizacion","H1":"Prom. Hora Pico","I1":"Prom. Picos Diarios","J1":"Total ONT","K1":"Servicios Datos","L1":"Servicios Empresariales","M1":"Empresariales de RBS"}
+encabezados_PON_uplink = {"A1":"Modelo Nodo","B1":"Nodo","C1":"Slot/Puerto","D1":"Hora Pico","E1":"Fecha Pico","F1":"Pico","G1":"% Utilizacion","H1":"Prom. Hora Pico","I1":"Prom. Picos Diarios"}
+encabezados_ONT = {"A1":"Modelo Nodo","B1":"Nodo","C1":"Slot/Puerto/ONT","D1":"Etiqueta","E1":"Hora Pico","F1":"Fecha Pico","G1":"Pico","H1":"% Utilizacion","I1":"Prom. Hora Pico","J1":"Prom. Picos Diarios"}
+
+hojas_PON = [DefiniciónEncabezados("Subida PON", encabezados_PON_PON), DefiniciónEncabezados("Bajada PON", encabezados_PON_PON), DefiniciónEncabezados("Subida Uplink", encabezados_PON_uplink), DefiniciónEncabezados("Bajada Uplink", encabezados_PON_uplink)]
+hojas_ONT = [DefiniciónEncabezados("Subida ONT", encabezados_ONT), DefiniciónEncabezados("Bajada ONT", encabezados_ONT)]
 
 
 #=========dato base datos========================#

8e0a8e1ebaddb5ee32ea9785d9406c6dc650c07e
Pelos17 <juavig@gmail.com>
2020-12-14 14:18:37 -0300

    Revert "Update tests.py"
    This reverts commit f2bcd19948aa10591e934c3fb92dd8973b44ed29.

--- reportes/Refactor/tests.py
+++ reportes/Refactor/tests.py
@@ -1,7 +1,6 @@
 from test2 import variable, otro, archivo
 import time
 
-''' SMART MENSAJE Y TIPO PARA FLUJOS'''
 class consultas():
     def __init__(self,query):
         self.query = query
@@ -14,23 +13,14 @@ class consultas():
         if self.query == "Hola":
             mensaje = "Mensaje Hoja"
             return mensaje
-''' ######################################'''
+
 
 #test = consultas("Hola")
 
 #print (test.query,test.sacar_tipo(),test.sacar_mensaje())
 
-# while True:
-#     print(variable())
-#     print(otro)
-#     print(archivo)
-#     time.sleep(5)
-
-lista = list(range(0,10))
-lista2 = list(range(11,20))
-
-for x in lista:
-    print (x)
-
-for x in lista2:
-    print (x)
+while True:
+    print(variable())
+    print(otro)
+    print(archivo)
+    time.sleep(5)

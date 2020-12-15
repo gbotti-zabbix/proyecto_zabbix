e04582f8099659c863b19a01f7460765d7ade789
Pelos17 <juavig@gmail.com>
2020-12-15 12:26:30 -0300

    Update orquestador.py

--- reportes/Refactor/orquestador.py
+++ reportes/Refactor/orquestador.py
@@ -8,7 +8,7 @@ import os
 from daemonize import Daemonize
 from direcciones import archivo_tlk, archivo_tlk_dst, archivo_tlk_viejo, archivo_rbs_DCS, archivo_rbs_DCS_dst, archivo_rbs_DCS_old, crudozabbix, limpiar_pickle_pon, limpiar_pickle_ont, pid
 from datetime import datetime
-from pusheo import f_cargar_inv_en_BD, pusheo_crudos_diarios_PON, pusheo_crudos_diarios_ONT
+from pusheo import f_cargar_inv_en_BD, pusheo_crudos_diarios_PON, pusheo_crudos_diarios_ONT, f_procesar_resumne_tlk_BD
 from parseo import parseo_ont, parseo_pon, f_parsear_inventario,f_parseo_inventario_RBS
 from flujo_db import flujos
 from reporte import reportes_xlsx
@@ -60,7 +60,7 @@ def orquestador():
             #-----llamo a parser inventario RBS----#
             logger.info(f'Arvhivo inventario RBS encontrado: {archivo_rbs_DCS}')
             logger.info("\n>>>>>>>>>>COMIENZO PROCESAMIENTO INVENTARIO RBS<<<<<<<<<<<<")
-            f_parseo_inventario_RBS (archivo_rbs_DCS,path_files+archivo_rbs_DCS_dst,archivo_rbs_DCS_old)
+            f_parseo_inventario_RBS (archivo_rbs_DCS,archivo_rbs_DCS_dst,archivo_rbs_DCS_old)
 
             #----Cargo inventario RBS parseado a la BD---#
             #f_cargar_inv_en_BD(path_files+file_rbs_DCSk_dst)

2ed8cda0c5de925bba47d24fc7c50e0a48f5b644
Pelos17 <juavig@gmail.com>
2020-12-23 12:01:44 -0300

    Update consultas.py

--- reportes/Refactor/consultas.py
+++ reportes/Refactor/consultas.py
@@ -52,32 +52,41 @@ insert_respaldo_mensual_pon =  consultas("insert into respaldo_reporte_mensual(t
 #ONT
 insert_promedio_mensual_ont =  consultas("insert into promedio_mensual_ont(tipo,nodo,puerto,direccion,promedio_mes) select tipo,nodo, puerto, direccion, avg(pico) as promedio_mes from picos_diarios_mensual_ont group by tipo, nodo, puerto, direccion;","Flujo","Insertando datos en promedio_mensual_ont")
 insert_picos_mensual_ont =  consultas("insert into picos_mensual_ont (tipo, nodo, puerto, etiqueta, direccion, hora, fecha, promedio, pico) select t1.tipo, t1.nodo, t1.puerto, t1.etiqueta, t1.direccion, t1.hora, t1.fecha, t1.promedio, t1.pico from ( select t2.*, row_number() over (partition by t2.tipo, t2.nodo, t2.puerto, t2.direccion order by t2.pico desc) as rn from picos_diarios_mensual_ont t2) t1 where t1.rn =1;","Flujo","Insertando datos en picos_mensual_ont")
-insert_reporte_mensual_ont =  consultas("insert into reporte_mensual_ont(tipo,nodo,puerto,etiqueta,direccion,hora,fecha,promediohora,pico,promediomes) select pico.tipo, pico.nodo,pico.puerto,pico.etiqueta,pico.direccion,pico.hora,pico.fecha,pico.promedio,pico.pico, promedio.promedio_mes FROM promedio_mensual_ont promedio, picos_mensual_ont pico WHERE pico.tipo = promedio.tipo and pico.nodo = promedio.nodo and pico.puerto = promedio.puerto and pico.direccion = promedio.direccion GROUP BY pico.tipo, pico.nodo, pico.puerto, pico.direccion;","Flujo","Insertando datos en reporte_mensual_ont")
-insert_respaldo_mensual_ont =  consultas("insert into respaldo_reporte_mensual_ont(tipo,nodo,puerto,etiqueta,direccion,hora,fecha,promediohora,pico,promediomes) select tipo,nodo,puerto,etiqueta,direccion,hora,fecha,promediohora,pico,promediomes FROM reporte_mensual_ont;","Flujo","Insertando datos en respaldo_reporte_mensual_ont")
+insert_promedio_hora_mensual_ont = consultas("insert into promedio_picos_promedio_mensual_ont(tipo,nodo,puerto,direccion,promedio_picos_promedio_mes) select tipo,nodo, puerto, direccion, avg(promedio) as promedio_picos_promedio_mes from picos_diarios_promedio_mensual_ont group by tipo, nodo, puerto, direccion;","Flujo","Insertando datos en promedio_picos_promedio_mensual_ont")
+insert_reporte_mensual_ont =  consultas("insert into reporte_mensual_ont(tipo,nodo,puerto,etiqueta,direccion,hora,fecha,promediohora,promediomes,pico,promedio_picos_promedios) select pico.tipo,pico.nodo,pico.puerto,pico.etiqueta,pico.direccion,pico.hora,pico.fecha,pico.promedio, promedio.promedio_mes, pico.pico, p_promedio.promedio_picos_promedio_mes FROM promedio_mensual_ont promedio, picos_mensual_ont pico, promedio_picos_promedio_mensual_ont p_promedio WHERE pico.tipo = promedio.tipo and pico.nodo = promedio.nodo and pico.puerto = promedio.puerto and pico.direccion = promedio.direccion and pico.tipo = p_promedio.tipo and pico.nodo = p_promedio.nodo and pico.puerto = p_promedio.puerto and pico.direccion = p_promedio.direccion GROUP BY pico.tipo, pico.nodo, pico.puerto, pico.direccion;","Flujo","Insertando datos en reporte_mensual_ont")
+insert_respaldo_mensual_ont =  consultas("insert into respaldo_reporte_mensual_ont(tipo,nodo,puerto,etiqueta,direccion,hora,fecha,promediohora,promediomes,pico,promedio_picos_promedios) select tipo,nodo,puerto,etiqueta,direccion,hora,fecha,promediohora,promediomes,pico, promedio_picos_promedios FROM reporte_mensual_ont;","Flujo","Insertando datos en respaldo_reporte_mensual_ont")
 
 ###Truncates###
 ##Truncates semanales##
 #PON
 truncate_picos_diarios_semanal_pon = consultas('TRUNCATE picos_diarios_semanal;',"Truncate","Truncando datos en picos_diarios_semanal")
+truncate_picos_diarios_promedio_semanal_pon = consultas('TRUNCATE picos_diarios_promedio_semanal;',"Truncate","Truncando datos en picos_diarios_promedio_semanal")
 truncate_promedio_semanal_pon = consultas('TRUNCATE promedio_semanal;',"Truncate","Truncando datos en promedio_semanal")
 truncate_picos_semanal_pon = consultas('TRUNCATE picos_semanal;',"Truncate","Truncando datos en picos_semanal")
+truncate_promedio_picos_promedio_semanal_pon = consultas('TRUNCATE promedio_picos_promedio_semanal;',"Truncate","Truncando datos en promedio_picos_promedio_semanal")
 truncate_reporte_semanal_pon = consultas('TRUNCATE reporte_semanal;',"Truncate","Truncando datos en reporte_semanal")
 #ONT
 truncate_picos_diarios_semanal_ont = consultas('TRUNCATE picos_diarios_semanal_ont;',"Truncate","Truncando datos en picos_diarios_semanal_ont")
+truncate_picos_diarios_promedio_semanal_ont = consultas('TRUNCATE picos_diarios_promedio_semanal_ont;',"Truncate","Truncando datos en picos_diarios_promedio_semanal_ont")
 truncate_promedio_semanal_ont = consultas('TRUNCATE promedio_semanal_ont;',"Truncate","Truncando datos en promedio_semanal_ont")
 truncate_picos_semanal_ont = consultas('TRUNCATE picos_semanal_ont;',"Truncate","Truncando datos en picos_semanal_ont")
+truncate_promedio_picos_promedio_semanal_ont = consultas('TRUNCATE promedio_picos_promedio_semanal_ont;',"Truncate","Truncando datos en promedio_picos_promedio_semanal_ont")
 truncate_reporte_semanal_ont = consultas('TRUNCATE reporte_semanal_ont;',"Truncate","Truncando datos en reporte_semanal_ont")
 
 ##Truncates mensuales##
 #PON
 truncate_picos_diarios_mensual_pon = consultas('TRUNCATE picos_diarios_mensual;',"Truncate","Truncando datos en picos_diarios_mensual")
+truncate_picos_diarios_promedio_mensual_pon = consultas('TRUNCATE picos_diarios_promedio_mensual;',"Truncate","Truncando datos en picos_diarios_promedio_mensual")
 truncate_promedio_mensual_pon = consultas('TRUNCATE promedio_mensual;',"Truncate","Truncando datos en promedio_mensual")
 truncate_picos_mensual_pon = consultas('TRUNCATE picos_mensual;',"Truncate","Truncando datos en picos_mensual")
+truncate_promedio_picos_promedio_mensual_pon = consultas('TRUNCATE promedio_picos_promedio_mensual;',"Truncate","Truncando datos en promedio_picos_promedio_mensual")
 truncate_reporte_mensual_pon = consultas('TRUNCATE reporte_mensual;',"Truncate","Truncando datos en reporte_mensual")
 #ONT
 truncate_picos_diarios_mensual_ont = consultas('TRUNCATE picos_diarios_mensual_ont;',"Truncate","Truncando datos en picos_diarios_mensual_ont")
+truncate_picos_diarios_promedio_mensual_ont = consultas('TRUNCATE picos_diarios_promedio_mensual_ont;',"Truncate","Truncando datos en picos_diarios_promedio_mensual_ont")
 truncate_promedio_mensual_ont = consultas('TRUNCATE promedio_mensual_ont;',"Truncate","Truncando datos en promedio_mensual_ont")
 truncate_picos_mensual_ont = consultas('TRUNCATE picos_mensual_ont;',"Truncate","Truncando datos en picos_mensual_ont")
+truncate_promedio_picos_promedio_mensual_ont = consultas('TRUNCATE promedio_picos_promedio_mensual_ont;',"Truncate","Truncando datos en promedio_picos_promedio_mensual_ont")
 truncate_reporte_mensual_ont = consultas('TRUNCATE reporte_mensual_ont;',"Truncate","Truncando datos en reporte_mensual_ont")
 
 ###Deletes-Respaldos###
@@ -90,7 +99,7 @@ delete_respaldo_reporte_mensual_ont = consultas('delete from respaldo_reporte_me
 
 ##Flujos##
 #Diario
-flujo_diario = [insert_picos_diarios_semanal_pon,insert_picos_diarios_mensual_pon,insert_picos_diarios_semanal_ont,insert_picos_diarios_mensual_ont]
+flujo_diario = [insert_picos_diarios_semanal_pon,insert_picos_diarios_hora_semanal_pon,insert_picos_diarios_mensual_pon,insert_picos_diarios_hora_mensual_pon,insert_picos_diarios_semanal_ont,insert_picos_diarios_hora_semanal_ont,insert_picos_diarios_mensual_ont,insert_picos_diarios_hora_mensual_ont]
 #Semanales
 flujo_semanal_pon = [truncate_promedio_semanal_pon,truncate_picos_semanal_pon,truncate_reporte_semanal_pon,insert_promedio_semanal_pon,insert_picos_semanal_pon,insert_reporte_semanal_pon,insert_respaldo_semanal_pon,truncate_picos_diarios_semanal_pon]
 flujo_semanal_ont = [truncate_promedio_semanal_ont,truncate_picos_semanal_ont,truncate_reporte_semanal_ont,insert_promedio_semanal_ont,insert_picos_semanal_ont,insert_reporte_semanal_ont,insert_respaldo_semanal_ont,truncate_picos_diarios_semanal_ont]

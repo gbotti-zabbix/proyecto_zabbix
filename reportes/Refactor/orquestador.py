7dd4a948b76b0c588f2b8113d19ca3ac5369db45
Pelos17 <juavig@gmail.com>
2021-01-05 10:49:21 -0300

    Update orquestador.py

--- reportes/Refactor/orquestador.py
+++ reportes/Refactor/orquestador.py
@@ -39,65 +39,68 @@ def checkdia():
     
 
 def orquestador():
-    while True:
-        # existe archivo TLK #
-        if checkFileExistance(archivo_tlk):
-            #-----llamo a parser inventario tlk----#
-            logger.info(f'Arvhivo inventario TLK encontrado: {archivo_tlk}')
-            logger.info("\n>>>>>>>>>>COMIENZO PROCESAMIENTO INVENTARIO TELELINK<<<<<<<<<<<<")
-            f_parsear_inventario (archivo_tlk,archivo_tlk_dst,archivo_tlk_viejo)
+    try:
+        while True:
+            # existe archivo TLK #
+            if checkFileExistance(archivo_tlk):
+                #-----llamo a parser inventario tlk----#
+                logger.info(f'Arvhivo inventario TLK encontrado: {archivo_tlk}')
+                logger.info("\n>>>>>>>>>>COMIENZO PROCESAMIENTO INVENTARIO TELELINK<<<<<<<<<<<<")
+                f_parsear_inventario (archivo_tlk,archivo_tlk_dst,archivo_tlk_viejo)
 
-            #----Cargo inventario tlk parseado a la BD---#
-            f_cargar_inv_en_BD(archivo_tlk_dst)
+                #----Cargo inventario tlk parseado a la BD---#
+                f_cargar_inv_en_BD(archivo_tlk_dst)
 
-            #--- Proceso BD inventario tlk-----#
-            f_procesar_resumne_tlk_BD()
-            logger.info(">>>>>>>>>>FIN PROCESAMIENTO INVENTARIO TELELINK<<<<<<<<<<<<\n\n")
-        #if fin existe archivo TLK #
+                #--- Proceso BD inventario tlk-----#
+                f_procesar_resumne_tlk_BD()
+                logger.info(">>>>>>>>>>FIN PROCESAMIENTO INVENTARIO TELELINK<<<<<<<<<<<<\n\n")
+            #if fin existe archivo TLK #
 
-        # existe archivo RBS DSC #
-        elif checkFileExistance(archivo_rbs_DCS):
-            #-----llamo a parser inventario RBS----#
-            logger.info(f'Arvhivo inventario RBS encontrado: {archivo_rbs_DCS}')
-            logger.info("\n>>>>>>>>>>COMIENZO PROCESAMIENTO INVENTARIO RBS<<<<<<<<<<<<")
-            f_parseo_inventario_RBS (archivo_rbs_DCS,archivo_rbs_DCS_dst,archivo_rbs_DCS_old)
+            # existe archivo RBS DSC #
+            elif checkFileExistance(archivo_rbs_DCS):
+                #-----llamo a parser inventario RBS----#
+                logger.info(f'Arvhivo inventario RBS encontrado: {archivo_rbs_DCS}')
+                logger.info("\n>>>>>>>>>>COMIENZO PROCESAMIENTO INVENTARIO RBS<<<<<<<<<<<<")
+                f_parseo_inventario_RBS (archivo_rbs_DCS,archivo_rbs_DCS_dst,archivo_rbs_DCS_old)
 
-            #----Cargo inventario RBS parseado a la BD---#
-            #f_cargar_inv_en_BD(path_files+file_rbs_DCSk_dst)
+                #----Cargo inventario RBS parseado a la BD---#
+                #f_cargar_inv_en_BD(path_files+file_rbs_DCSk_dst)
 
-            #--- Proceso BD inventario tlk-----#
-            logger.info(">>>>>>>>>>FIN PROCESAMIENTO INVENTARIO RBS<<<<<<<<<<<<\n\n")
-        #if fin existe archivo TLK #        
-        # existe archivo Zabbix #
-        elif checkFileExistance(crudozabbix()):
-            #Parseo archivo de Zabbix PON y ONT
-            parseo_ont()
-            parseo_pon()
-            #Borro crudozabbix
-            os.remove(crudozabbix())
-            logger.info("Se borro archivo crudozabbix")
-            #pusheo pickles de ONT y PON
-            pusheo_crudos_diarios_PON()
-            pusheo_crudos_diarios_ONT()
-            #borra crudos pickle viejos ONT y PON
-            os.system(limpiar_pickle_pon)
-            os.system(limpiar_pickle_ont)
-            #Ejecuto funciones sql diarias")
-            flujos("dia")
-            if checklunes() == 1:
-                #Ejecuto funcione sql semanal")
-                flujos("semana")
-                #Saco reporte semanal")
-                reportes_xlsx("PON","semana")
-                reportes_xlsx("ONT","semana")
-            if checkdia() == 1:
-                #Ejecuto funcione sql mensual")
-                flujos("mes")
-                #Saco reporte mensual")
-                reportes_xlsx("PON","mes")
-                reportes_xlsx("ONT","mes")
-        else:
-            time.sleep(1200)
+                #--- Proceso BD inventario tlk-----#
+                logger.info(">>>>>>>>>>FIN PROCESAMIENTO INVENTARIO RBS<<<<<<<<<<<<\n\n")
+            #if fin existe archivo TLK #        
+            # existe archivo Zabbix #
+            elif checkFileExistance(crudozabbix()):
+                #Parseo archivo de Zabbix PON y ONT
+                parseo_ont()
+                parseo_pon()
+                #Borro crudozabbix
+                os.remove(crudozabbix())
+                logger.info("Se borro archivo crudozabbix")
+                #pusheo pickles de ONT y PON
+                pusheo_crudos_diarios_PON()
+                pusheo_crudos_diarios_ONT()
+                #borra crudos pickle viejos ONT y PON
+                os.system(limpiar_pickle_pon)
+                os.system(limpiar_pickle_ont)
+                #Ejecuto funciones sql diarias")
+                flujos("dia")
+                if checklunes() == 1:
+                    #Ejecuto funcione sql semanal")
+                    flujos("semana")
+                    #Saco reporte semanal")
+                    reportes_xlsx("PON","semana")
+                    reportes_xlsx("ONT","semana")
+                if checkdia() == 1:
+                    #Ejecuto funcione sql mensual")
+                    flujos("mes")
+                    #Saco reporte mensual")
+                    reportes_xlsx("PON","mes")
+                    reportes_xlsx("ONT","mes")
+            else:
+                time.sleep(1200)
+    except Exception as e:
+        logger.error(traceback.format_exc())
 
 #-----main----#
 

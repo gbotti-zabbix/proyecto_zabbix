817192db6b910fe5d9442dab0e7b8f1a04748a13
Pelos17 <juavig@gmail.com>
2021-01-05 17:50:56 -0300

    Update test.py

--- carga_ont/test.py
+++ carga_ont/test.py
@@ -1,30 +1,6 @@
 import requests
 import json
 
-#r = requests.get('https://api.github.com/events')
-
-#r = requests.post('https://httpbin.org/post', data = {'key':'value'})
-
-#print(r.text)
-'''
-payload = {'key1': 'value1', 'key2': ['value2', 'value3']}
-payload2= {
-    "jsonrpc": "2.0",
-    "method": "item.get",
-    "params": {
-        "output": ["name","key_"],
-        "search": {
-            "key_": "PONRX[zxAnPonOnuIfRxOctets.ONT17/1/4]"
-        },
-        "sortfield": "name"
-    },
-    "auth": "codigoauthdelogin.json",
-    "id": 1
-}
-r = requests.get('https://httpbin.org/get', params=payload2)
-print(r.url)
-'''
-
 url = "http://10.0.0.101/zabbix/api_jsonrpc.php"
 payload= {
     "jsonrpc": "2.0",

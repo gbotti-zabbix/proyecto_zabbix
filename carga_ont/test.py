529e1e4d250f47949bf0b43f3cd7ad23a9bc50be
Juan Vignolo - Laptop <juavig@gmail.com>
2021-01-07 15:35:16 -0300

    Update test.py

--- carga_ont/test.py
+++ carga_ont/test.py
@@ -1,38 +1,12 @@
 import requests
 import json
 
-proxies = {"192.168.211.4:80":"http://f030014:brisingr05@proxy.in.iantel.com.uy:80"}
+from direcciones import url
+from llamadas import host_get 
 
-url = "http://192.168.211.4/zabbix/api_jsonrpc.php"
+host_get("Tuviega")
+
 '''
-payload= {
-    "jsonrpc": "2.0",
-    "method": "item.get",
-    "params": {
-        "output": ["name","key_"],
-        "search": {
-            "key_": "PONRX[zxAnPonOnuIfRxOctets.ONT17/1/4]"
-        },
-        "sortfield": "name"
-    },
-    "auth": "23f8f52facd8cfc607d92862d38d669a",
-    "id": 1
-}
-'''
-
-payload = {
-    "jsonrpc": "2.0",
-    "method": "user.login",
-    "params": {
-        "user": "jvignolo",
-        "password": "brisingr"
-    },
-    "id": 1,
-    "auth": "null"
-}
-encabezado = {"Content-Type": "application/json-rpc"}
-
-
 r = requests.post(url,proxies=proxies,headers=encabezado,json=payload)
 
 print(r.status_code)
@@ -50,3 +24,4 @@ print(r.status_code)
 
 #if testeo == nombre:
     #print("Tal Cual")
+'''

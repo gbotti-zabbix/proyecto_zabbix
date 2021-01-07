09422f56f6c11adf6a3a91ba5d7ed5aef0a37138
Pelos17 <juavig@gmail.com>
2021-01-07 12:38:52 -0300

    Create testconection.py

---
+++ carga_ont/testconection.py
@@ -0,0 +1,10 @@
+import requests
+import urllib
+
+proxies = {"http://192.168.211.4":"http://f030014:brisingr05@proxy.in.iantel.com.uy:80"}
+proxies2 = {"http": "http://f030014:brisingr05@proxy.in.iantel.com.uy:80","https": "https://f030014:brisingr05@proxy.in.iantel.com.uy:80"}
+encabezado = {"Content-Type": "application/json-rpc"}
+
+r = requests.get("http://192.168.211.4/zabbix/api_jsonrpc.php",headers=encabezado,proxies=urllib.request.getproxies())
+
+print(r.status_code)

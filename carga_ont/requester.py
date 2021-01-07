6882b2dfb31c5e63662924ec159bf4f6e6c30601
Juan Vignolo - Laptop <juavig@gmail.com>
2021-01-07 16:02:15 -0300

    Create requester.py

---
+++ carga_ont/requester.py
@@ -0,0 +1,7 @@
+url = "http://10.0.0.101/zabbix/api_jsonrpc.php"
+
+encabezado = {"Content-Type": "application/json-rpc"}
+
+def requester(payload):
+    r = requests.post(url,headers=encabezado,json=payload)
+    return r

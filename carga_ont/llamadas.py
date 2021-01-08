c3505642fc6676ace4a6835ee99d7f3318c86ab6
Juan Vignolo - Laptop <juavig@gmail.com>
2021-01-08 11:49:33 -0300

    Update llamadas.py

--- carga_ont/llamadas.py
+++ carga_ont/llamadas.py
@@ -10,7 +10,7 @@ def ont_check(opcion,parametro,auth):
     "jsonrpc": "2.0",
     "method": "item.get",
     "params": {
-        "output": ["name","key_"],
+        "output": "{}".opcion,
         "search": {
             "{}".format(opcion): "{}".format(parametro)
         },

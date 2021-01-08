3b597759547dba94ddf67c07d16273984f6b63c1
Juan Vignolo - Laptop <juavig@gmail.com>
2021-01-08 17:00:47 -0300

    Update orquestador_carga_ont.py

---
+++ carga_ont/orquestador_carga_ont.py
@@ -0,0 +1,40 @@
+from llamadas import ont_check, host_get, get_inter_id, get_app_id, get_oid
+from sesiones import autorizar, logout
+
+
+def orquestador_carga_ont():
+    create_ont = {
+        "jsonrpc": "2.0",
+        "method": "item.create",
+        "params": {
+            "name": "A_TEST_ONT",
+            "key_": "TEST_KEY",
+            "hostid": "11288",
+            "interfaceid": "1363",
+        "type": 4,
+            "value_type": "3",
+            "snmp_community": "{$SNMP_COMMUNITY}",
+        "snmp_oid": "TEST OID",
+        "units": "bps",
+            "delay": "5m",
+            "applications": [
+                "71245"
+            ],
+        "preprocessing": [
+                {
+                    "type": "10",
+            "params":""
+                },
+            {
+            "type": "1",
+            "params": 8
+            }
+        ]
+        },
+        "auth": "23f8f52facd8cfc607d92862d38d669a",
+        "id": 1
+    }
+    print(create_ont)
+
+
+orquestador_carga_ont()

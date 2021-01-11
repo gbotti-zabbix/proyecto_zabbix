aa0452979d9a86c2ca94f614ded4655ee9c98a8c
Juan Vignolo - Laptop <juavig@gmail.com>
2021-01-11 15:02:49 -0300

    Update llamadas.py

--- carga_ont/llamadas.py
+++ carga_ont/llamadas.py
@@ -302,11 +302,12 @@ def create_ont(nombre,llave,hostid,interfaceid,oid,appid,auth):
         "id": 1
     }
     create_ont = requester(create_ont)
-    if len(create_ont.json()["result"]) < 1:
-        print("No se creo la ONT: {}".format(nombre))
-        return 0
-    else:
-        print(create_ont.json()["result"])
-        return 1
+    print(create_ont.json())
+    # if len(create_ont.json()["result"]) < 1:
+    #     print("No se creo la ONT: {}".format(nombre))
+    #     return 0
+    # else:
+    #     print(create_ont.json()["result"])
+    #     return 1
 
-create_ont("TEST_ONT","TEST_LLAVE","11288","1363","TEST OID","71245","fc6c4c0d30ed633e8dd173e4f69e628b")
+create_ont("AAA_TEST_ONT","TEST_LLAVE","11288","1363","TEST OID","71245","fc6c4c0d30ed633e8dd173e4f69e628b")

b8a9522ee51b897177d31ddf8dbaefcc561a531f
Juan Vignolo - Laptop <juavig@gmail.com>
2021-01-12 16:19:26 -0300

    Update orquestador_carga_ont.py

--- carga_ont/orquestador_carga_ont.py
+++ carga_ont/orquestador_carga_ont.py
@@ -17,8 +17,9 @@ def orquestador_carga_ont(metodo):
             modelo = rbs[0]
             nodo = rbs[1]
             puerto = str(rbs[2]) + "/" + str(rbs[3]) + "/" + str(rbs[4])
-            key = "PONTX[zxAnPonOnuIfRxOctets.ONT{}".format(puerto)
+            key = "PONTX[zxAnPonOnuIfRxOctets.ONT{}]".format(puerto)
             print(puerto)
+            print(key)
             chequeo = ont_check("key_",key,llave)
             if modelo != "C300":
                 descarte = descarte +1

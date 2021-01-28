ff1551a48eb40314abf76fc0facc2fdb7bb5e553
Pelos17 <juavig@gmail.com>
2021-01-28 16:10:52 -0300

    Update orquestador_carga_ont.py

--- carga_ont/orquestador_carga_ont.py
+++ carga_ont/orquestador_carga_ont.py
@@ -1,13 +1,22 @@
 from llamadas import get_rbs, ont_check, host_get, get_inter_id, get_app_id, get_oid, create_ont, get_name, get_zabbix_key
 from sesiones import autorizar, logout
 
+#IMPORTO LOGGER DE REFACTOR
+import sys
+sys.path.append("./reportes/Refactor")
+import logger
+
+
+
 #METODO MANUAL O AUTOMATICO (manual es ingreso a mano)
 
 def orquestador_carga_ont(metodo):  
     if metodo == "manual":
+        logger.info("Comienza la auditoria manual de ONTs")
         #TENGO QUE LLAMAR A LAS FUNCIOENS CON INPUTS
         pass
     elif metodo == "auto":
+        logger.info("Comienza la auditoria automatica de ONTs")
         lista = []
         repetidas = []
         faltante = 0
@@ -15,6 +24,7 @@ def orquestador_carga_ont(metodo):
         descarte = 0
         llave = autorizar()
         lista_rbs = get_rbs()
+        contador_break = 0
         for rbs in lista_rbs:
             nodo = rbs[0]
             puerto = str(rbs[2]) + "/" + str(rbs[3]) + "/" + str(rbs[4])
@@ -46,22 +56,29 @@ def orquestador_carga_ont(metodo):
                 zkey = get_zabbix_key(puerto)
                 comparador = str(nodo)+str(zkey["RX"])+str(hostid)
                 appid = get_app_id(hostid,llave)
-                print(appid)
                 if comparador in lista:
                     repetidas.append(comparador)
                 else:
                     chequeo = ont_check("key_",hostid,zkey["RX"],llave)
                     #aca crearia la lista de las ont no creadas y crearia las que corresponde.
                     if chequeo == 0:
+                        if contador_break >= 3:
+                            break
                         print(nodo,zkey)
                         print(nombre)
                         print("******")
                         lista.append(comparador)
                         faltante = faltante + 1
+                        #Tiene que crear un ITEM PARA TX y OTRO PARA CX
+                        create_ont(nombre["RX"],zkey["RX"],hostid,inter_id,oid["oid_rx"],appid,llave)
+                        create_ont(nombre["TX"],zkey["TX"],hostid,inter_id,oid["oid_tx"],appid,llave)
+                        logger.info("Se crearon los item de ONT {} en el nodo {}".format(nombre,nodo))
+                        contador_break = contador_break + 1
                     elif chequeo == 1:
                         encontrado = encontrado + 1
                         lista.append(comparador)
 
+        logger.info("{} ONTs encontradas,{} repetidas, {} sin encontrar y {} descartadas".format(encontrado,len(repetidas),faltante,descarte))
         print("{} ONTs encontradas,{} repetidas, {} sin encontrar y {} descartadas".format(encontrado,len(repetidas),faltante,descarte))
         logout(llave)
         print(repetidas)

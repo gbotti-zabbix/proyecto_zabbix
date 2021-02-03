85a7ea09adf4234aab5da866b8c50eb2eed7495b
Juan Vignolo - Laptop <juavig@gmail.com>
2021-02-03 15:48:28 -0300

    Create auditorias_ont.py

---
+++ auditorias/auditorias_ont.py
@@ -0,0 +1,49 @@
+
+from consultas import get_rbs_tlk, get_rbs
+
+from datetime import datetime
+
+def auditar_ont():
+    lista_g = get_rbs()
+    lista_tlk = get_rbs_tlk()
+    nlista_g = []
+    nlista_tlk = []
+    diferenciatlk = []
+    diferenciag = []
+    for rbs in lista_g:
+        dato = rbs[0] + "_" + str(rbs[2]) + "/" + str(rbs[3]) + "/" + str(rbs[4])
+        nlista_g.append(dato)
+    for rbs in lista_tlk:
+        dato = rbs[0] + "/" + str(rbs[1])
+        nlista_tlk.append(dato)
+    for rbs in nlista_tlk:
+        if rbs in nlista_g:
+            pass
+        else:
+            diferenciatlk.append(rbs)
+    for rbs in nlista_g:
+        if rbs in nlista_tlk:
+            pass
+        else:
+            diferenciag.append(rbs)
+    with open("/var/lib/reportes-zabbix/auditoria_ont.txt","w") as archivo:
+        archivo.write("Comienza auditoria ONTs. Fecha {}".fromat(datetime.now))
+        archivo.write("\n")
+        archivo.write("\n")
+        for rbs in diferenciatlk:
+            archivo.write(rbs)
+            archivo.write("\n")
+        archivo.write("{} ONTs en TLK no se encontraron el listado de gestion.".format(len(diferenciatlk)))
+        archivo.write("\n")        
+        archivo.write("######")
+        archivo.write("\n")
+        archivo.write("\n")
+        for rbs in diferenciag:
+            archivo.write(rbs)
+            archivo.write("\n")
+        archivo.write("{} ONTs en Gestion no se encontraron el listado de TLK.".format(len(diferenciag)))
+        archivo.write("\n")
+        archivo.write("######")
+        archivo.write("\n")
+        archivo.write("\n")
+        archivo.write("Finalizo auditoria ONTs. Fecha {}".fromat(datetime.now))

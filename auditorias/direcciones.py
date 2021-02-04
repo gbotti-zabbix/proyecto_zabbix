d0d43bd400865cdd398403a2a80745e7b846ef14
Juan Vignolo - Laptop <juavig@gmail.com>
2021-02-04 13:41:38 -0300

    Update direcciones.py

--- auditorias/direcciones.py
+++ auditorias/direcciones.py
@@ -1,3 +1,5 @@
 auditoria_ont = "/var/lib/reportes-zabbix/auditorias/auditoria_ont.txt"
 
 auditoria_pon = "/var/lib/reportes-zabbix/auditorias/auditoria_pon.txt"
+
+file_log = "/var/log/auditorias.log"

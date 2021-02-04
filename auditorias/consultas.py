ebacf3d7e5c60a47df5b4818a1d808b77de8df69
Juan Vignolo - Laptop <juavig@gmail.com>
2021-02-04 13:35:44 -0300

    Update consultas.py

--- auditorias/consultas.py
+++ auditorias/consultas.py
@@ -12,3 +12,13 @@ def get_rbs_tlk():
     sql ="SELECT `id_tlk`, `nro_ont` FROM `t_reporte_puertos_telelink` WHERE `rbs_ont_tlk` > 0"
     rbs = conector(sql,"select","Consultando ONTS de TLK")
     return rbs
+
+def get_puertos_pon_tlk():
+    sql ="SELECT `id_tlk` FROM `t_resumen_servicios_tlk`"
+    rbs = conector(sql,"select","Consultando Puertos PON TLK")
+    return rbs
+
+def get_puertos_pon_zbx():
+    sql ="SELECT `nodo`, `puerto`FROM `reporte_semanal` WHERE `direccion` = 'RX'"
+    rbs = conector(sql,"select","Consultando Puertos PON Zabbix")
+    return rbs

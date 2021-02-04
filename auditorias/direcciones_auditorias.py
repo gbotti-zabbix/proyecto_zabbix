fd0651bbc5ecc64afb0fe153967d94a5f011ffcd
Juan Vignolo - Laptop <juavig@gmail.com>
2021-02-04 15:35:28 -0300

    Update direcciones_auditorias.py

--- auditorias/direcciones_auditorias.py
+++ auditorias/direcciones_auditorias.py
@@ -1,3 +1,11 @@
 auditoria_ont = "/var/lib/reportes-zabbix/auditorias/auditoria_ont.txt"
 
 auditoria_pon = "/var/lib/reportes-zabbix/auditorias/auditoria_pon.txt"
+
+puertos_uplink = ["21/1","22/1","21/2","21/3","21/4","22/2","22/3","22/4"]
+
+puertos_uplink_h = ["9/0","10/0"]
+
+puertos_uplink_19 = ["19/1","20/1","19/2","19/3","19/4","20/2","20/3","20/4"]
+
+c300_19p = ["G-AZNAREZ-01Z", "MARISCALA-01Z", "A-ARENA-01Z"]

1d8683d4ebe0ca679606be525866425987a2578a
Pelos17 <juavig@gmail.com>
2021-02-05 12:47:29 -0300

    Update orquestador_carga_ont.py

--- carga_ont/orquestador_carga_ont.py
+++ carga_ont/orquestador_carga_ont.py
@@ -23,11 +23,11 @@ def orquestador_carga_ont(metodo):
         puerto = input("Ingrese Slot/Puerto/ONT:\nEjemplo: 17/1/4\n")
         llave = autorizar()
         hostid = host_get(nodo,llave)
+        inter_id = get_inter_id(hostid,llave)
         try:
-            inter_id = get_inter_id(hostid,llave)
-        except Exception as e:
+            ip = inter_id["ip"]
+        except NoneType as e:
             print("El nodo {} no existe en Zabbix".format(nodo))
-        ip = inter_id["ip"]
         oid = get_oid("zte",puerto)
         opcion_e = input("Ingrese 1 para ingresar etitquetas, sino precione enter para continuar\n")
         if opcion_e == "1":

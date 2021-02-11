5b5a2d905cbd171fd9bb20775bcbebc18c81eac7
Pelos17 <juavig@gmail.com>
2021-02-11 15:54:41 -0300

    Update reporte.py

--- reportes/Working/reporte.py
+++ reportes/Working/reporte.py
@@ -96,10 +96,10 @@ def reportes_xlsx(tipo,periodo):
             apend_data_ONT(workbook,hojas_ONT,periodo)
             if periodo == "mes":
                 workbook.save(filename=excel_ONT_mensual())
-                os.system("chmod 775 {}".format(excel_ONT_mensual))
+                #os.system("chmod 775 {}".format(excel_ONT_mensual))
             elif periodo == "semana":
                 workbook.save(filename=excel_ONT_semanal())
-                os.system("chmod 775 {}".format(excel_ONT_semanal))
+                #os.system("chmod 775 {}".format(excel_ONT_semanal))
             logger.info("Se culmino la creacion del reporte {} de ONT".format(periodo))
 
     elif tipo == "PON":
@@ -109,13 +109,13 @@ def reportes_xlsx(tipo,periodo):
             apend_data_PON(workbook,hojas_PON,periodo)
             if periodo == "mes":
                 workbook.save(filename=excel_PON_mensual())
-                os.system("chmod 775 {}".format(excel_PON_semanal))
+                #os.system("chmod 775 {}".format(excel_PON_semanal))
             elif periodo == "semana":
                 workbook.save(filename=excel_PON_semanal())
-                os.system("chmod 775 {}".format(excel_PON_semanal))
+                #os.system("chmod 775 {}".format(excel_PON_semanal))
             logger.info("Se culmino la creacion del reporte {} de PON".format(periodo))
 
-reportes_xlsx("PON","semana")
-reportes_xlsx("ONT","semana")
-reportes_xlsx("PON","mes")
-reportes_xlsx("ONT","mes")
+#reportes_xlsx("PON","semana")
+#reportes_xlsx("ONT","semana")
+#reportes_xlsx("PON","mes")
+#reportes_xlsx("ONT","mes")

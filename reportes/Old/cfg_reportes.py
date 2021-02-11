96f3e6f9007df30cd3c3bc00666081dd6e4d65f6
Juan Vignolo - Laptop <juavig@gmail.com>
2021-02-11 11:42:17 -0300

    Create cfg_reportes.py

---
+++ reportes/Old/cfg_reportes.py
@@ -0,0 +1,26 @@
+
+
+#========variables globales=========
+
+#---   LOGS  --------------
+file_logs= "/var/log/reportes_zabbix/proc_tlk_BD.log"
+
+
+#=======  Archivos inventarios y procesados Tlk=========
+
+# -directorio origen y destino de archivos
+path_files="/var/lib/reportes-zabbix/reporte_tlk/"  #directorio trabajo
+
+file_tlk="PLN245_procesado.TXT"               #nombre archivo origen
+file_tlk_dst="PLN245_pareseado.csv"              #nombre archivo destino
+file_tlk_old="PLN245_procesado.old.TXT"              #luego parseo renombro archivo original
+
+
+
+
+#=========dato base datos========================
+#-variables para conexión a la base de datos
+host_DB="localhost"
+user_DB="reportes"
+password_DB="antel2020"
+database_DB="reportes_zabbix"

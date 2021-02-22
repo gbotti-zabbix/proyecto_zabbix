373c8b880d38df420c020a3ee04523116d0029c3
Juan Vignolo - Laptop <juavig@gmail.com>
2021-02-22 14:09:51 -0300

    Update orquestador_reportes_manual.py

--- reportes/Working/orquestador_reportes_manual.py
+++ reportes/Working/orquestador_reportes_manual.py
@@ -135,7 +135,7 @@ def menu():
 
 
 
-with open("/var/lib/reportes-zabbix/crudos/Merged-Trends-" + str(date.today()) + ".pickle","r") as archivo:
+with open("/var/lib/reportes-zabbix/crudos/Merged-Trends-" + str(date.today()) + ".pickle","rb") as archivo:
     lista_tuplas = pickle.load(archivo)
     for lista in lista_tuplas:
         print(lista)

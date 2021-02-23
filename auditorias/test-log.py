2e55c328e0e982d57b824f4740419cdc6986cf62
Juan Vignolo - Laptop <juavig@gmail.com>
2021-02-23 11:31:32 -0300

    Update test-log.py

--- auditorias/test-log.py
+++ auditorias/test-log.py
@@ -4,6 +4,7 @@ import time
 with open("/var/log/reportes_zabbix/test.log","a") as archivo:
     for i in range(0,10000000):
         numero = random.randrange(200,1000)
-        time.sleep(30)
-        archivo.write(numero)
+        print(numero)
+        archivo.write(str(numero))
         archivo.write("\n")
+        time.sleep(30)

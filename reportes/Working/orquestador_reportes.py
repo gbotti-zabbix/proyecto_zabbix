5623110aad7c44094dfd98800ea19bd04d95c564
pelos <juavig@gmail.com>
2021-02-26 16:55:32 -0300

    Update orquestador_reportes.py

--- reportes/Working/orquestador_reportes.py
+++ reportes/Working/orquestador_reportes.py
@@ -13,8 +13,45 @@ from pusheo import f_cargar_inv_en_BD,f_cargar_inv_RBS_en_BD, pusheo_crudos_diar
 from parseo import parseo_ont, parseo_pon, f_parsear_inventario, f_parseo_inventario_RBS
 from flujo_db import flujos
 from reporte import reportes_xlsx
+""" Parseo, pusheo, flujos en la BD y generacion de reportes PON/ONT.
 
+Este script se encarga de llamar a todas las funciones que son pertinentes a la creacion
+de los reportes a partir de info de Zabbix, Telelink y Gestion.
+
+Las funciones llamadas detectan crudos a parsear, los pushean a la BD y generan los flujos
+de consultas necesarioas para crear datos utiles para los reportes. Por ultimo crean los
+archivos finales de reportes consultando la BD.
+
+Se utilizan funciones que controlan dias y fechas para decidir que procedimientos ejecutar.
+
+El orquestador solo tiene la funcion de coordinar los distintos eslabones de los reportes.
+Las funcionalidades finas deben editarse en las funciones importadas y llamadas desde
+orquestador_reportes().
+
+Una ves ejecutado el script, se genera un daemon de linux con su correspondiente PID y Socket,
+por lo que no captura la terminal desde la que se ejecuta. Para detener el script se debe matar
+el proceso con su correspondiente PID.
+
+Contiene las funciones:
+    * checkFileExistance - Chequea que el archivo crudo a parsear exista.
+    * checklunes - Chequea que sea lunes.
+    * checkdia - Chequea que sea primero de mes.
+    * orquestador_reportes -  Orquesta la llamada a las funciones encargadas
+    de todo el proceso de generacion de reportes. Parseo de curdos, pusheos, etc.
+    * deamon - Genera un daemon desde el codigo python de orquestador.
+"""
 def checkFileExistance(filePath):
+    """ Chequea la existencia de un archivo
+
+    Recibe un directorio como string. Si encuentra el archivo devuelve
+    True, de lo contrario devuelve False.
+
+    :param filePath: Directorio en formato str a chequear.
+    :type filePath: str
+
+    :returns: True o Flase dependiendo si encontro el archivo o no.
+    :rtpye: bool
+    """
     try:
         with open(filePath, 'r') as f:
             logger.info("Se encontro {}".format(filePath))
@@ -26,6 +63,14 @@ def checkFileExistance(filePath):
 
 
 def checklunes():
+    """ Chequea si es Lunes
+
+    Comprueba si al momento de llamar la funcion es Lunes.
+    De serlo devuelve 1, de lo contrario 0
+
+    :returns: 1 o 0 dependiendo si es lunes o no.
+    :rtpye: int
+    """
     if datetime.today().weekday() == 0:
         return 1
     else:
@@ -33,6 +78,14 @@ def checklunes():
 
 
 def checkdia():
+    """ Chequea si es primero de mes
+
+    Comprueba si al momento de llamar la funcion es primero de mes.
+    De serlo devuelve 1, de lo contrario 0
+
+    :returns: 1 o 0 dependiendo si es lunes o no.
+    :rtpye: int
+    """
     if datetime.now().strftime("%d") == "01":
         return 1
     else:
@@ -40,6 +93,51 @@ def checkdia():
     
 
 def orquestador_reportes():
+    """ Gestiona/llama todas las funciones relevante a los reportes
+
+    Comienza con un ciclo while de 30 min de sleep entre ciclo. En cada ciclo se chequea
+    la existencia de archivos crudos de TLK, gestion o Zabbix. De encontrar alguno, comienza
+    las tareas de ingreso de crudos diarios, flujos de datos en al BD y/o generacion de reportes.
+
+    * Si encuentra archivo TLK:
+        Logea que encontro el archivo.
+
+        Se llama al funcion f_parsear_inventario pasando los nombres de archivos a generar.
+
+        Luego se carga el parseo diario a la BD con la funcion f_cargar_inv_en_BD.
+
+        Por ultimo ejecuta las consultas en la BD que generan la informacion util para los
+        reportes y se logea la finalizacion del proceso
+
+    * Si encuentra el archivo de Gestion:
+        Logea que encontro el archivo.
+
+        Se llama al funcion f_parseo_inventario_RBS pasando los nombres de archivos a generar.
+
+        Luego se carga el parseo diario a la BD con la funcion f_cargar_inv_RBS_en_BD.
+
+        Se logea la finalizacion del proceso
+
+    * Si encuentra el archivo de Zabbix:
+        Se llaman a las funciones de parseo tanto para ONT como PON. Una ves que finalizan se borra
+        el archivo crudo (NDJSON), y se logea esto ultimo.
+
+        Se pushean los crudos diarios de PON y ONT a la BD.
+
+        Se limpian archivos pickle con mas de 30 dias.
+
+        Se llaman a los flujos de la BD que debene realizarse todos los dias.
+
+        Si checklunes retorna 1, se ejecutan flujos de BD correspondientes a la semana y
+        se crean los reportes .xlsx de ONT y PON.
+
+        Si checkdia retorna 1, se ejecutan flujos de BD correspondientes a la semana y
+        se crean los reportes .xlsx de ONT y PON.
+    
+    Por ultimo se captura cualquier exepcion en el proceso y se logea.
+    
+    :returns: Esta funcion no tiene retornos.
+    """
     try:
         while True:
             # existe archivo TLK #
@@ -111,6 +209,6 @@ def orquestador_reportes():
 #orquestador()
 #demonio
 
-
+#Se ejecuta el script actual en forma de daemon
 daemon = Daemonize(app="orquestador_reportes", pid=pid, action=orquestador_reportes)
 daemon.start()

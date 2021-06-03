e4fb5467295e428f6243bbd77da26ac47ed6bd66
Pelos17 <juavig@gmail.com>
2021-06-03 11:54:14 -0300

    Update direcciones.py

--- reportes/Working/direcciones.py
+++ reportes/Working/direcciones.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python
 
-#HOLA ESTOY PROBANDO ESTA NUEVA FUNCION
-def ():
-    break
-
 from datetime import date
 
 """ ###Direcciones de archivos o datos hardcodeados utilizados para todos los demas scripts.
